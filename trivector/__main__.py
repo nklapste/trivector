e949d15a58fc0feed4933c7aad059e6fee1fdabe
Nathan Klapstein <nklapste@ualberta.ca>
2018-05-29 19:07:37 -0400

    refactoring
    adding docstrings
    using enum `DiagonalStyle`
    further functioning

--- trivector/__main__.py
+++ trivector/__main__.py
@@ -7,34 +7,33 @@ import os
 import argparse
 import sys
 
-from trivector import trivector
+from trivector.trivector import trivector, DiagonalStyle
 
 
 def main():
     """argparse function for trivector"""
     parser = argparse.ArgumentParser()
-    parser.add_argument("image", required=True, help="Path to the image to trivector")
+    parser.add_argument("image", help="Path to the image to trivector")
     parser.add_argument("-o", "--output", default=os.path.join(os.getcwd(), "<image_name>_tri_<cut_size>.<svg & png>"),
-                        help="Path to output the trivectored image (default: %(default)s)")
+                        help="Path to output the trivectored image "
+                             "(default: %(default)s)")
 
     group = parser.add_argument_group("Image Generation Options")
     group.add_argument("-c", "--cut-size", dest="cut_size", type=int, required=True,
-                       help="Size in pixels for each triangle sector (smaller==more triangles)")
-
-    group = parser.add_argument_group("Image Diagonal Options")
-    group = group.add_mutually_exclusive_group()
-    group.add_argument("--right-diagonal", dest="right_diagonal", action="store_true",
-                       help="Triangle sectors will be only orientated to the right `/`")
-    group.add_argument("--left-diagonal", dest="left_diagonal", action="store_true",
-                       help="Triangle sectors will be only orientated to the left `\\`")
+                       help="Size in pixels for each triangle sector "
+                            "(smaller==more triangles)")
+    group.add_argument("-d", "--diagonal-style", dest="diagonal_style",
+                       type=DiagonalStyle, choices=list(DiagonalStyle),
+                       default=DiagonalStyle.alternating.value,
+                       help="Styling on how to arrange the triangle "
+                            "sectors diagonals (default: %(default)s)")
 
     args = parser.parse_args()
 
     trivector(
         image_path=args.image,
         cut_size=args.cut_size,
-        right_diagonal=args.right_diagonal,
-        left_diagonal=args.left_diagonal,
+        diagonal_style=args.diagonal_style
     )
 
     return 0
--- trivector/trivector.py
+++ trivector/trivector.py
@@ -1,21 +1,34 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-""""""
+"""Convert an image into a vector image composed of triangles"""
 
 import os
+from enum import Enum
+
 import numpy
-from numpy import ndarray
+from numpy import ndarray, array
+
 import svgwrite
+from svgwrite import Drawing
+
 import cv2
 import progressbar
 from cairosvg import svg2png
 
 
-def upper_tri_sum(d3array: ndarray) -> numpy.array:
-    """"""
-    x, y, z = d3array.shape
+def upper_tri_sum(d3array: ndarray) -> array:
+    """Get a 3D image array's upper diagonal's pixel color average
 
+    :param d3array: a 3D image array derived from :func:`cv2.imread`
+
+    Treat the 3D array as 2d array. Having the innermost array (pixel BGR
+    values) be considered base values to be averaged.
+
+    :return: a BGR array of the average color of the upper diagonal of the
+        3D image array
+    """
+    x, y, z = d3array.shape
     tri = []
     for i in range(x):
         if i > y:
@@ -25,8 +38,22 @@ def upper_tri_sum(d3array: ndarray) -> numpy.array:
     return numpy.sum(tri, axis=0) // len(tri)
 
 
-def lower_tri_sum(d3array: ndarray) -> numpy.array:
-    """"""
+def lower_tri_sum(d3array: ndarray) -> array:
+    """Get a 3D image array's lower diagonal's pixel color average
+
+    :param d3array: a 3D image array derived from :func:`cv2.imread`
+
+    Treat the 3D array as 2d array. Having the innermost array (pixel BGR
+    values) be considered base values to be averaged.
+
+    .. note::
+
+        If the lower diagonal cannot be computed (eg: flat/malformed 3D array)
+        use the 3D image array's upper diagonal's pixel color average instead.
+
+    :return: a BGR array of the average color of the lower diagonal of the
+        3D image array
+    """
     x, y, z = d3array.shape
     tri = []
     for i in range(x):
@@ -35,86 +62,120 @@ def lower_tri_sum(d3array: ndarray) -> numpy.array:
         for j in range(i):
             tri.append(d3array[i][j])
 
-    # if bottom tri is empty fill it with the top tri's data
+    # if bottom tri is use the upper tri's sum
     if not tri:
         return upper_tri_sum(d3array)
     return numpy.sum(tri, axis=0) // len(tri)
 
 
-def trivector(image_path: str, cut_size: int, right_diagonal: bool = False, left_diagonal: bool = False, output_path: str = None):
-    """"""
+def vectorize_sector_left(sub_img: ndarray, dwg: Drawing,
+                     x: int, y: int, cut_size: int):
+    """Add two triangles to ``dwg`` whose colors are derived from the color
+    averages from the top and bottom diagonals of the 3D BGR image array of
+    the sub image"""
+    b, g, r = upper_tri_sum(sub_img)
+    dwg.add(
+        dwg.polygon(
+            [(x, y), (x + cut_size, y), (x + cut_size, y + cut_size)],
+            stroke=svgwrite.rgb(r, g, b, "RGB"),
+            fill=svgwrite.rgb(r, g, b, "RGB")
+        )
+    )
+    b, g, r = lower_tri_sum(sub_img)
+    dwg.add(
+        dwg.polygon(
+            [(x, y), (x, y + cut_size), (x + cut_size, y + cut_size)],
+            stroke=svgwrite.rgb(r, g, b, "RGB"),
+            fill=svgwrite.rgb(r, g, b, "RGB")
+        )
+    )
+
+
+def vectorize_sector_right(sub_img: ndarray, dwg: Drawing,
+                           x: int, y: int, cut_size: int):
+    """Add two triangles to ``dwg`` whose colors are derived from the color
+    averages from the top and bottom diagonals of the 3D BGR image array of
+    the sub image"""
+    b, g, r = upper_tri_sum(sub_img)
+    dwg.add(
+        dwg.polygon(
+            [(x, y + cut_size), (x + cut_size, y + cut_size), (x + cut_size, y)],
+            stroke=svgwrite.rgb(r, g, b, "RGB"),
+            fill=svgwrite.rgb(r, g, b, "RGB")
+        )
+    )
+
+    b, g, r = lower_tri_sum(sub_img)
+    dwg.add(
+        dwg.polygon(
+            [(x, y + cut_size), (x, y), (x + cut_size, y)],
+            stroke=svgwrite.rgb(r, g, b, "RGB"),
+            fill=svgwrite.rgb(r, g, b, "RGB")
+        )
+    )
+
+
+class DiagonalStyle(Enum):
+    """Styling noting on how to arrange the triangle sectors diagonals"""
+    right = "right"
+    left = "left"
+    alternating = "alternating"
+
+    def __str__(self):
+        return self.value
+
+
+def trivector(image_path: str, cut_size: int, output_path: str = None,
+              diagonal_style: DiagonalStyle = DiagonalStyle.alternating):
+    """Convert an image into a vector image composed of triangles
+
+    Save the vector image as both a ``.svg`` and ``.png``.
+
+    :param image_path: path to the image to convert to a vector image
+    :param cut_size: size in pixels for each triangle sector
+        (smaller==more triangles)
+    :param diagonal_style: a :class:`DiagonalStyle` styling noting on how to
+        arrange the triangle sectors diagonals
+    :param output_path: path to write the output images to
+    """
     img = cv2.imread(image_path)
     image_name = os.path.basename(image_path)
-    image_name, ext = os.path.splitext(image_name)
+    image_name, _ = os.path.splitext(image_name)
 
     if not output_path:
-        output_path = os.path.join(os.getcwd(), "{}_tri_{}.svg".format(image_name, cut_size))
-    dwg = svgwrite.Drawing(output_path, profile="full")
+        output_path = os.path.join(os.getcwd(), "{}_tri_{}".format(image_name, cut_size))
+    dwg = svgwrite.Drawing(output_path+".svg", profile="full")
 
     height, width, channels = img.shape
 
     width_slices = range(0, width, cut_size)
     height_slices = range(0, height, cut_size)
     dwg.viewbox(width=len(width_slices)*cut_size, height=len(height_slices)*cut_size)
+
+    # start up the progress bar
+    # each image sector is one tick one the progress bar
     bar = progressbar.ProgressBar(max_value=len(width_slices)*len(height_slices))
     counter2 = 0
     sector_num = 0
-
-
     for y in height_slices:
+
         counter = counter2
         for x in width_slices:
 
             sub_img = img[y:y + cut_size, x:x + cut_size]
-            if left_diagonal or (not right_diagonal and counter % 2):
-                tri_color = upper_tri_sum(sub_img)
-                b, g, r = tri_color
-                dwg.add(
-                    dwg.polygon(
-                        [(x, y), (x + cut_size, y), (x + cut_size, y + cut_size)],
-                        stroke=svgwrite.rgb(r, g, b, 'RGB'),
-                        fill=svgwrite.rgb(r, g, b, 'RGB')
-                    )
-                )
-                tri_color = lower_tri_sum(sub_img)
-                b, g, r = tri_color
-                dwg.add(
-                    dwg.polygon(
-                        [(x, y), (x, y + cut_size), (x + cut_size, y + cut_size)],
-                        stroke=svgwrite.rgb(r, g, b, 'RGB'),
-                        fill=svgwrite.rgb(r, g, b, 'RGB')
-                    )
-                )
-            else:
-                # inverting orientation of tris
+            if (diagonal_style == DiagonalStyle.left) or \
+                    (diagonal_style == DiagonalStyle.alternating and counter % 2):
+                vectorize_sector_left(sub_img, dwg, x, y, cut_size)
+            elif (diagonal_style == DiagonalStyle.right) or \
+                    (diagonal_style == DiagonalStyle.alternating and not counter % 2):
                 sub_img = numpy.rot90(sub_img, axes=(0, 1))
-                tri_color = upper_tri_sum(sub_img)
-                b, g, r = tri_color
-                dwg.add(
-                    dwg.polygon(
-                        [(x, y+cut_size), (x + cut_size, y), (x + cut_size, y + cut_size)],
-                        stroke=svgwrite.rgb(r, g, b, 'RGB'),
-                        fill=svgwrite.rgb(r, g, b, 'RGB')
-                    )
-                )
-
-                tri_color = lower_tri_sum(sub_img)
-                b, g, r = tri_color
-                dwg.add(
-                    dwg.polygon(
-                        [(x, y), (x, y + cut_size), (x + cut_size, y)],
-                        stroke=svgwrite.rgb(r, g, b, 'RGB'),
-                        fill=svgwrite.rgb(r, g, b, 'RGB')
-                    )
-                )
+                vectorize_sector_right(sub_img, dwg, x, y, cut_size)
+            else:
+                raise TypeError("diagonal_style is invalid")
             sector_num += 1
             bar.update(sector_num)
             counter += 1
         counter2 += 1
+
     dwg.save()
-    filename, file_extension = os.path.splitext(output_path)
-    svg2png(open(output_path, 'rb').read(), write_to=open(filename+os.extsep+"png", 'wb'))
-
-
-if __name__ == "__main__":
-    trivector("../images/me2.png", 45)
+    svg2png(open(output_path+".svg", "rb").read(), write_to=open(output_path+".png", "wb"))
