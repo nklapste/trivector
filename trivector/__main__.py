803578611b21937bc6905fd6f516bbfc426a79f7
Nathan Klapstein <nklapste@ualberta.ca>
2018-05-29 17:41:49 -0400

    adding help to argparse statements

--- trivector/__main__.py
+++ trivector/__main__.py
@@ -3,6 +3,7 @@
 
 """argparse script for trivector"""
 
+import os
 import argparse
 import sys
 
@@ -12,17 +13,20 @@ from trivector import trivector
 def main():
     """argparse function for trivector"""
     parser = argparse.ArgumentParser()
-    parser.add_argument("image", help="Path to the image to trivector")
-    parser.add_argument("-o", "--output", help="Path to output the trivectored image (default: %(default)s)")
+    parser.add_argument("image", required=True, help="Path to the image to trivector")
+    parser.add_argument("-o", "--output", default=os.path.join(os.getcwd(), "<image_name>_tri_<cut_size>.<svg & png>"),
+                        help="Path to output the trivectored image (default: %(default)s)")
 
     group = parser.add_argument_group("Image Generation Options")
-    group.add_argument("-c", "--cut-size", dest="cut_size", type=int, default=15,
-                       help="")  # TODO: help message
+    group.add_argument("-c", "--cut-size", dest="cut_size", type=int, required=True,
+                       help="Size in pixels for each triangle sector (smaller==more triangles)")
 
     group = parser.add_argument_group("Image Diagonal Options")
     group = group.add_mutually_exclusive_group()
-    group.add_argument("--right-diagonal", dest="right_diagonal", action="store_true", help="")  # TODO
-    group.add_argument("--left-diagonal", dest="left_diagonal", action="store_true", help="")  # TODO
+    group.add_argument("--right-diagonal", dest="right_diagonal", action="store_true",
+                       help="Triangle sectors will be only orientated to the right `/`")
+    group.add_argument("--left-diagonal", dest="left_diagonal", action="store_true",
+                       help="Triangle sectors will be only orientated to the left `\\`")
 
     args = parser.parse_args()
 
