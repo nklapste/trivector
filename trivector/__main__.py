561eaf20ac030f10fa30246c3083dadf935a597c
Nathan Klapstein <nklapste@ualberta.ca>
2018-07-04 17:51:32 -0400

    using vectorizer adding stroke options

--- trivector/__main__.py
+++ trivector/__main__.py
@@ -3,14 +3,21 @@
 
 """argparse script for trivector"""
 
-import os
 import argparse
 import sys
 
-from trivector.trivector import trivector, DiagonalStyle
+from trivector.vectorizer import Vectorizer, Style
 
 
-def get_parser():
+def rgb(s):
+    try:
+        r, g, b = map(int, tuple(s))
+        return r, g, b
+    except:
+        raise argparse.ArgumentTypeError("RGB values must be r,g,b")
+
+
+def get_parser() -> argparse.ArgumentParser:
     """Create and return the argparser for trivector"""
     parser = argparse.ArgumentParser(
         description="Convert an image into a ``.svg`` vector image composed "
@@ -19,19 +26,25 @@ def get_parser():
     parser.add_argument("image", help="Path to the image to trivector")
 
     group = parser.add_argument_group("Image Generation Options")
-    group.add_argument("-o", "--output", default=os.path.join(
-                       ".", "<image_name>_tri_<cut_size>.svg"),
-                       help="Path to output the trivectored image "
+    group.add_argument("-o", "--output-path", dest="output_path",
+                       default="unnamed.svg",
+                       help="Path to output the vectorized image "
                             "(default: %(default)s)")
     group.add_argument("-c", "--cut-size", dest="cut_size", type=int,
                        required=True,
-                       help="Size in pixels for each triangle sector "
-                            "(smaller==more triangles)")
-    group.add_argument("-d", "--diagonal-style", dest="diagonal_style",
-                       type=DiagonalStyle, choices=list(DiagonalStyle),
-                       default=DiagonalStyle.alternating.value,
-                       help="Styling on how to arrange the triangle "
-                            "sectors diagonals (default: %(default)s)")
+                       help="Size in pixels for each vectorized sector "
+                            "(smaller==more sectors)")
+    group.add_argument("-s", "--style", dest="style",
+                       type=Style, choices=list(Style),
+                       default=Style.tri_alternating.value,
+                       help="Styling to use to compose the vectorized "
+                            "sectors (default: %(default)s)")
+    group.add_argument("--stroke-color", dest="stroke_color",
+                       default=None, type=rgb,
+                       help="(default: %(default)s)")
+    group.add_argument("-stroke-width", dest="stroke_width",
+                       type=float, default=0.0,
+                       help="(default: %(default)s)")
     return parser
 
 
@@ -40,23 +53,16 @@ def main(argv=sys.argv[1:]):
     parser = get_parser()
     args = parser.parse_args(argv)
 
-    # generate or get the output image name
-    image_name = os.path.basename(args.image)
-    image_name, _ = os.path.splitext(image_name)
-    if not args.output:
-        output_path = os.path.join(
-            os.getcwd(),
-            "{}_tri_{}.svg".format(args.image, args.cut_size)
-        )
-    else:
-        output_path = args.output
-
-    trivector(
+    # create run and save with the Vectorizer
+    vectorizer = Vectorizer(
         image_path=args.image,
         cut_size=args.cut_size,
-        output_path=output_path,
-        diagonal_style=args.diagonal_style
+        output_path=args.output_path,
+        style=args.style,
+        stroke_width=args.stroke_width
     )
+    vectorizer.run()
+    vectorizer.save()
 
     return 0
 
