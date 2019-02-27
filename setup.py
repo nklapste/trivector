c2e3ee0b4c0dbf3917af9d5e917ffe30e2865c7b
Nathan Klapstein <nklapste@ualberta.ca>
2019-02-27 10:02:43 -0700

    adding better project setup structure
    using travis tests

--- setup.py
+++ setup.py
@@ -24,9 +24,17 @@ VERSION = find_version("trivector", "__init__.py")
 
 
 class Pylint(test):
+    user_options = [('pylint-args=', 'a', "Arguments to pass to pylint")]
+
+    def initialize_options(self):
+        test.initialize_options(self)
+        self.pylint_args = "trivector --persistent=y --rcfile=.pylintrc --output-format=colorized"
+
     def run_tests(self):
+        import shlex
+        # import here, cause outside the eggs aren't loaded
         from pylint.lint import Run
-        Run(["trivector", "--persistent", "y"])
+        Run(shlex.split(self.pylint_args))
 
 
 class PyTest(test):
@@ -45,7 +53,7 @@ class PyTest(test):
 
 
 def readme():
-    with open("README.rst") as f:
+    with open("README.rst", encoding="utf-8") as f:
         return f.read()
 
 
@@ -57,28 +65,27 @@ setup(
     author="Nathan Klapstein",
     author_email="nklapste@ualberta.ca",
     url="https://github.com/nklapste/trivector",
-    packages=find_packages(exclude=["test"]),
-    include_package_data=True,
-    package_data={
-        "": ["README.rst"],
-    },
-    entry_points={
-        "console_scripts": [
-            "trivector = trivector.__main__:main"
-        ]
-    },
+    license="MIT",
     classifiers=[
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Topic :: Multimedia :: Graphics :: Editors :: Vector-Based",
         "Topic :: Multimedia :: Graphics :: Graphics Conversion",
     ],
+    packages=find_packages(exclude=["test"]),
+    include_package_data=True,
     install_requires=[
         "svgwrite>=1.1.12,<2.0.0",
         "opencv-python>=3.4.1.15,<4.0.0.0",
         "numpy>=1.14.3,<2.0.0",
         "progressbar2>=3.37.1,<4.0.0",
+    ],
+    tests_requires=[
+        "pytest>=2.8.7,<4.0.0",
+        "pytest-cov<=2.6.0",
+        "pylint>=1.9.1,<2.0.0",
     ],
     extras_require={
         "docs": [
@@ -87,12 +94,11 @@ setup(
             "sphinx-autodoc-typehints>=1.3.0,<2.0.0",
             "sphinx-argparse>=0.2.2,<1.0.0",
         ],
-        "tests": [
-            "pytest",
-            "pytest-cov",
-            "pytest-timeout",
-            "pylint>=1.9.1,<2.0.0",
-        ]
     },
     cmdclass={"test": PyTest, "lint": Pylint},
+    entry_points={
+        "console_scripts": [
+            "trivector = trivector.__main__:main"
+        ]
+    },
 )
