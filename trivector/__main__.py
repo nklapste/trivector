c92f068ab68a7e8ec1261a8559df5e9c132578f8
Nathan Klapstein <nklapste@ualberta.ca>
2019-02-27 10:44:00 -0700

    restructuring `__main__` cli

--- trivector/__main__.py
+++ trivector/__main__.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""argparse script for trivector"""
+"""argparse and entry point script for trivector"""
 
-import os
 import argparse
 import sys
 
@@ -13,48 +12,32 @@ from trivector.trivector import trivector, DiagonalStyle
 def get_parser():
     """Create and return the argparser for trivector"""
     parser = argparse.ArgumentParser(
-        description="Convert an image into a ``.svg`` vector image composed "
-                    "of triangles"
+        description="Convert an image into a SVG vector image composed "
+                    "of triangular sectors",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-    parser.add_argument("image", help="Path to the image to trivector")
+    parser.add_argument("image", help="path to the image to trivector")
+    parser.add_argument("output", help="path to output the trivectored image")
 
     group = parser.add_argument_group("Image Generation Options")
-    group.add_argument("-o", "--output", default=os.path.join(
-                       ".", "<image_name>_tri_<cut_size>.svg"),
-                       help="Path to output the trivectored image "
-                            "(default: %(default)s)")
-    group.add_argument("-c", "--cut-size", dest="cut_size", type=int,
-                       required=True,
-                       help="Size in pixels for each triangle sector "
-                            "(smaller==more triangles)")
+    group.add_argument("sector_size", type=int,
+                       help="size in pixels for each triangle sector")
     group.add_argument("-d", "--diagonal-style", dest="diagonal_style",
                        type=DiagonalStyle, choices=list(DiagonalStyle),
                        default=DiagonalStyle.alternating.value,
-                       help="Styling on how to arrange the triangle "
-                            "sectors diagonals (default: %(default)s)")
+                       help="diagonal arrangement of the triangle sectors")
     return parser
 
 
 def main(argv=sys.argv[1:]):
-    """argparse function for trivector"""
+    """main entry point for trivector"""
     parser = get_parser()
     args = parser.parse_args(argv)
 
-    # generate or get the output image name
-    image_name = os.path.basename(args.image)
-    image_name, _ = os.path.splitext(image_name)
-    if not args.output:
-        output_path = os.path.join(
-            os.getcwd(),
-            "{}_tri_{}.svg".format(args.image, args.cut_size)
-        )
-    else:
-        output_path = args.output
-
     trivector(
         image_path=args.image,
-        cut_size=args.cut_size,
-        output_path=output_path,
+        cut_size=args.sector_size,
+        output_path=args.output,
         diagonal_style=args.diagonal_style
     )
 
